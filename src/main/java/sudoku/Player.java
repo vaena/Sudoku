83af69ea0992c83aa209d23016b3078c8c8655f8
geomih <geo.mih93@gmail.com>
2014-01-04 16:16:54 +0200

    Sudoku

--- src/main/java/sudoku/Player.java
+++ src/main/java/sudoku/Player.java
@@ -3,54 +3,31 @@ package sudoku;
 
 /**
  * Abstract class, υπάρχει απλά για την περίπτωση επέκτασης που θέλουμε να επιλύει το computerplayer αυτόματα προβλήματα
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
  */
 
 public abstract class Player extends PersonPlayer
 {
-	/**
-	 * το nickname του χρήστη<div><br></div>
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+
 	
 	private String nickname;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
 	public Player(){
 		super();
 	}
 
 	/**
 	 * αλλάζει το nickname
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+         * @param parameter
 	 */
-	
 	public void setNickname(String parameter) {
 		// TODO : to implement	
 	}
 	
 	/**
-	 * μας επιστρέφει το nickname<div><br></div>
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * μας επιστρέφει το nickname
+         * @return 
 	 */
-	
 	public String getNickname() {
-		// TODO : to implement
 		return "";	
 	}
 	
